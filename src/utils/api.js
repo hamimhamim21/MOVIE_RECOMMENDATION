a67ab3870afc15c5c8762599836437479fe92d30
Hamim Shaikh <hamim.shaikh@healthcarelogic.com>
2023-09-03 20:40:17 +1000

    Pushing the movix website

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,29 +4,18 @@ const BASE_URL = "https://api.themoviedb.org/3";
 const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
 
 const headers = {
-  Authorization: "Bearer " + TMDB_TOKEN,
+    Authorization: "bearer " + TMDB_TOKEN,
 };
 
-async function fetchDataFromApi(endpoint, params) {
-  try {
-    const response = await axios.get(`${BASE_URL}${endpoint}`, {
-      headers: headers,
-      params: params, // Pass the params object to the axios config
-    });
-    return response.data;
-
-    // Use the response data here
-  } catch (error) {
-    console.error("Error fetching data:", error.message);
-  }
-}
-
-// Call the function with the specific endpoint and params to initiate the request
-const specificEndpoint = "your-endpoint"; // Replace 'your-endpoint' with the desired endpoint
-const queryParams = {
-  // Replace these with the specific query parameters you want to use
-  page: 1,
-  language: "en-US",
-  // Add more parameters as needed
+export const fetchDataFromApi = async (url, params) => {
+    try {
+        const { data } = await axios.get(BASE_URL + url, {
+            headers,
+            params,
+        });
+        return data;
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
 };
-export default fetchDataFromApi;
